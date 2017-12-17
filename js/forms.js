fffda01205bcb99796dd8b25f235fb6dfd0317c7
yuriigr <yuriy.grebinyuk@gmail.com>
2017-12-18 01:25:03 +0200

    new draft

--- js/forms.js
+++ js/forms.js
@@ -78,6 +78,10 @@
 	$(document).bind('articleLoaded', function() {
 		init();
 	});
+  
+//    $(document).bind('gcLoaded', function() {
+//		init();
+//	});
 
 })();
 // ----------------------------------------------------------------------
@@ -243,4 +247,47 @@
 
 
 
+(function() {
+
+  //load work page
+  $(document).delegate('[data-load-work]', 'click', function() {
+    var href = $(this).attr('href');
+    var $target = $('[data-target="work"]');
+
+    $('body').addClass('page--load');
+
+    // $.get(href, function(response) {
+    setTimeout(function() {
+      var response = $('[data-source="work"]').html();
+      $target.html(response);
+
+      var $work = $target.find('.page-wrapper');
+
+      setTimeout(function() {
+        $('body').removeClass('page--load').addClass('page--loaded');
+        $work.one('bsTransitionEnd', function() {
+          $('body').addClass('page--showed');
+          //$('.social-likes').socialLikes();
+          $(document).trigger('articleLoaded'); ///
+        });
+      }, 100);
+
+      // });
+    }, 500);
+
+    return false;
+  });
+
+  //close work page
+  $(document).delegate('.page--work .btn--close', 'click', function() {
+    var $target = $('[data-target="work"]');
+    $('body').removeClass('page--showed').addClass('page--hide');
+    var $work = $target.find('.page-wrapper');
+    $work.one('bsTransitionEnd', function() {
+      $('body').removeClass('page--loaded page--hide');
+      $target.empty();
+    });
+  });
+
+})();
 //# sourceMappingURL=forms.js.map
